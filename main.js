e199cf67a072105f006e0ad1e26fe7ae4aff812e
mpxcode <mpxcode@gmail.com>
2016-07-08 04:50:36 +0100

    Update the viewport when browser window changes size

--- main.js
+++ main.js
@@ -4,15 +4,12 @@ import MapViewer from './geo/mapviewer';
 var [windowW, windowH] = [window.innerWidth, window.innerHeight];
 var center = [-0.105842, 51.532740];
 var mv = new MapViewer('viewer1', 0, 0, windowW, windowH, 13, center);
-mv.onResize(function(windowW, windowH) {
-    this.x = 0;
-    this.y = 0;
-    this.w = windowW;
-    this.h = windowH;
-    this.renderer.view.style.width = this.w + "px";
-    this.renderer.view.style.height = this.h + "px";
-    this.renderer.resize(this.w, this.h);
-});
 document.mv = mv; // so I can use chrome console
+
+/* Fired when browser window gets resized */
+window.addEventListener('resize', () => {
+    var [w, h] = [window.innerWidth, window.innerHeight];
+    mv.onResize(w, h);
+}, true);
 
 mv.render();
